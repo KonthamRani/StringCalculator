72e6be4edefcd5ad0b9ca009244768897c12ea26
KonthamRani <rani.kontham222@gmail.com>
2024-10-01 16:20:49 +0530

    Test: Failing test for single number returning itself.

--- src/test/java/com/rani/calculator/StringCalculatorTest.java
+++ src/test/java/com/rani/calculator/StringCalculatorTest.java
@@ -6,11 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
   
-	@Test
+	//test case for empty string
+	@Test	
 	public void testEmptyStringReturnsZero() {
 	    StringCalculator calculator = new StringCalculator();
 	    assertEquals(0, calculator.add(""));
 	}
+	//Test case for single number
+	@Test
+	public void testSingleNumberReturnsItself() {
+	    StringCalculator calculator = new StringCalculator();
+	    assertEquals(1, calculator.add("1"));
+	}
+
 
 
 }
