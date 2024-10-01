361312831da82e88892ece035c72455a8e26b614
KonthamRani <rani.kontham222@gmail.com>
2024-10-01 16:41:25 +0530

    Implemented code to handle newlines between numbers.

--- src/main/java/com/rani/calculator/StringCalculator.java
+++ src/main/java/com/rani/calculator/StringCalculator.java
@@ -6,8 +6,8 @@ public class StringCalculator {
 	    if (numbers.isEmpty()) {
 	        return 0;
 	    }
-	    //code for string containing numbers
-	    String[] numArray = numbers.split(",");
+	    //code for string containing numbers, /n ,
+	    String[] numArray = numbers.split("[,\n]");
 	    int sum = 0;
 	    for (String num : numArray) {
 	        sum += Integer.parseInt(num);
