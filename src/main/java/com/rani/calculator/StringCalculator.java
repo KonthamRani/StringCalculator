c0ad2b54ae8f760a92f60c0f1cd65092fbe502bf
KonthamRani <rani.kontham222@gmail.com>
2024-10-01 16:53:33 +0530

    Implemented exception handling for negative numbers.

--- src/main/java/com/rani/calculator/StringCalculator.java
+++ src/main/java/com/rani/calculator/StringCalculator.java
@@ -2,11 +2,13 @@ package com.rani.calculator;
 
 public class StringCalculator {
 	public int add(String numbers) {
+		String[] numsArray = numbers.split("[,\n]");
 		//code for empty string
 	    if (numbers.isEmpty()) {
 	        return 0;
 	    }
 	    //code for string containing different delimeters
+	    
 	    else if (numbers.startsWith("//")) {
 	        String delimiter = numbers.substring(2, 3);
 	        numbers = numbers.substring(4);
@@ -17,9 +19,26 @@ public class StringCalculator {
 	        }
 	        return sum;
 	    }
-	    String[] numArray = numbers.split("[,\n]");
+	    else
+	    {
+	    	int sum = 0;
+	        StringBuilder negatives = new StringBuilder();
+	        for (String num : numsArray) {
+	            int number = Integer.parseInt(num);
+	            if (number < 0) {
+	                if (negatives.length() > 0) {
+	                    negatives.append(",");
+	                }
+	                negatives.append(number);
+	            }
+	            sum += number;
+	        }
+	        if (negatives.length() > 0) {
+	            throw new IllegalArgumentException("negative numbers not allowed " + negatives);
+	        }
+	    }
 	    int sum = 0;
-	    for (String num : numArray) {
+	    for (String num : numsArray) {
 	        sum += Integer.parseInt(num);
 	    }
 	    return sum;
@@ -28,6 +47,6 @@ public class StringCalculator {
 	
   public static void main(String[] args) {
 	  StringCalculator calculator = new StringCalculator();
-      System.out.println(calculator.add("1,2"));
+      System.out.println(calculator.add("1,-2"));
   }
 }
