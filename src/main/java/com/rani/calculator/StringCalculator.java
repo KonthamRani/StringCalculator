ca64cb83bd08388c299d960f45bfb2938071a22d
KonthamRani <rani.kontham222@gmail.com>
2024-10-01 16:46:07 +0530

    Implemented code to support different delimiters.

--- src/main/java/com/rani/calculator/StringCalculator.java
+++ src/main/java/com/rani/calculator/StringCalculator.java
@@ -6,7 +6,17 @@ public class StringCalculator {
 	    if (numbers.isEmpty()) {
 	        return 0;
 	    }
-	    //code for string containing numbers, /n ,
+	    //code for string containing different delimeters
+	    else if (numbers.startsWith("//")) {
+	        String delimiter = numbers.substring(2, 3);
+	        numbers = numbers.substring(4);
+	        String[] numArray = numbers.split(delimiter);
+	        int sum = 0;
+	        for (String num : numArray) {
+	            sum += Integer.parseInt(num);
+	        }
+	        return sum;
+	    }
 	    String[] numArray = numbers.split("[,\n]");
 	    int sum = 0;
 	    for (String num : numArray) {
