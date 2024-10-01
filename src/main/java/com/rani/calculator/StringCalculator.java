2f9b4b09d7d289cea8d7bb93d2e3eb7c80670ee2
KonthamRani <rani.kontham222@gmail.com>
2024-10-01 16:31:08 +0530

    Implemented code to pass test for two numbers.

--- src/main/java/com/rani/calculator/StringCalculator.java
+++ src/main/java/com/rani/calculator/StringCalculator.java
@@ -3,13 +3,21 @@ package com.rani.calculator;
 public class StringCalculator {
 	public int add(String numbers) {
 		//code for empty string
-        if (numbers.isEmpty()) {
-            return 0;
-        }
-        //code for single number
-        return Integer.parseInt(numbers);
-        
-    }
+	    if (numbers.isEmpty()) {
+	        return 0;
+	    }
+	    String[] numArray = numbers.split(",");
+	    //code for single number
+	    if (numArray.length == 1) {
+	        return Integer.parseInt(numbers);
+	    } 
+	    //code for 1+ numbers
+	    else {
+	        return Integer.parseInt(numArray[0]) + Integer.parseInt(numArray[1]);
+	    }
+	}
+
+	
   public static void main(String[] args) {
 	  StringCalculator calculator = new StringCalculator();
       System.out.println(calculator.add("1"));
